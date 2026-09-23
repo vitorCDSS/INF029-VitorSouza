9805072b6211c5bab90a23c4117c106217e11083
vitorCDSS <vitorchagas2904@gmail.com>
2026-09-23 20:50:15 -0300

    projetoescola.c

--- projetoescola.c
+++ projetoescola.c
@@ -1,11 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define Max_alunos 40
-#define Max_professores 40
-#define Max_disciplinas 40#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
 #include<ctype.h>
 #include<time.h>
 #define Max_alunos 40
